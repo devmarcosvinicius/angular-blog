1ed8909269fa358ff84025839850a16d74f0b62a
Marcos Vinicius <contato@marcosviniciusdev.com>
2023-12-17 00:16:13 -0300

    feat: add dynamic components from database

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule } from "@angular/router"; // Remova RouterOutlet e Router
 import { AppComponent } from './app.component';
 import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
 import { MenuTitleComponent } from './components/menu-title/menu-title.component';
 import { BigCardComponent } from './components/big-card/big-card.component';
 import { SmallCardComponent } from './components/small-card/small-card.component';
 import { HomeComponent } from './pages/home/home.component';
+import { ContentComponent } from './pages/content/content.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -15,10 +17,13 @@ import { HomeComponent } from './pages/home/home.component';
     MenuTitleComponent,
     BigCardComponent,
     SmallCardComponent,
-    HomeComponent
+    HomeComponent,
+    ContentComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    RouterModule, // Adicione esta linha para importar RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,14 @@
+export  const dataFake = [
+  {
+    "id": "1",
+    "title": "Marvel vs DC",
+    "description": "Complicado isso ai.",
+    "photoCover": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS1fW-lGQDFUkNIk6G4ymlIZJD3k7qiYLrNiw&usqp=CAU"
+  },
+  {
+    "id": "2",
+    "title": "Heisenberg",
+    "description": "Say my name.",
+    "photoCover": "https://pbs.twimg.com/media/GBK4nGxWkAArVRK.jpg"
+  }
+]
